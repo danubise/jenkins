2cc965be94293987cc8bb6745c899dfc23b1fb4b
danubise <danubise@gmail.com>
2019-08-30 17:47:56 +0400

    updated shell script

--- lscommand.cmd
+++ lscommand.cmd
@@ -4,3 +4,8 @@ echo "test data" > somefile.txt
 data=$(cat somefile.txt)
 echo ${data}
 echo "data=$(echo ${data})" >> env.sh
+ls
+echo "test1 data" > somefile.txt
+data=$(cat somefile.txt)
+echo ${data}
+echo "data=$(echo ${data})" >> env.sh
