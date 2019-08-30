7ed5e3e5b32d3d55a5371e564aa12559cd2d094e
danubise <danubise@gmail.com>
2019-08-30 17:49:32 +0400

    updated shell script

--- lscommand.cmd
+++ lscommand.cmd
@@ -1,10 +1,5 @@
 ls
 touch somefile.txt
-echo "test data" > somefile.txt
-data=$(cat somefile.txt)
-echo ${data}
-echo "data=$(echo ${data})" >> env.sh
-ls
 echo "test1 data" > somefile.txt
 data=$(cat somefile.txt)
 echo ${data}
