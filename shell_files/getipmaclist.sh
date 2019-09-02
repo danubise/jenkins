4e489b75730e6b04788a6913fd63e896597b0e1d
danubise <danubise@gmail.com>
2019-09-02 17:57:31 +0400

    updated shell script

--- shell_files/getipmaclist.sh
+++ shell_files/getipmaclist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 printenv
-mkdir ~/archive/
+[ -d ~/archive ] || mkdirmkdir ~/archive/
 curl ${DOCUMENTURL} > ~/archive/${BUILD_ID}.csv && \
 rm -f ~/new.csv && \
 ln -s ~/archive/${BUILD_ID}.csv ~/new.csv && \
